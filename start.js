1f77cd3bef0e9c0343c944c85c858c283caf2b51
rikords <iamhakanduyar@gmail.com>
2024-03-16 00:28:05 +0300

    Adds a visual indicator for correct and incorrect answers.

--- start.js
+++ start.js
@@ -72,9 +72,13 @@ choices.forEach((choice) => {
 
     const classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-    console.log(classToApply);
 
-    getNewQuestions();
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestions();
+    }, 1000);
   });
 });
 
