00eb3acf942404b0cdc46fb2033aadfb4c13a477
rikords <iamhakanduyar@gmail.com>
2024-03-16 23:10:41 +0300

    Update frontend code to include score and username functionality.

--- start.js
+++ start.js
@@ -2,10 +2,12 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const timeText = document.getElementById("time");
+const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswer = false;
+let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let timeLeft = document.getElementById("time");
@@ -40,6 +42,8 @@ let questions = [
   },
 ];
 
+//CONSTANTS
+const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 //initial setup
@@ -62,6 +66,7 @@ const timerDisplay = () => {
 
 startQuiz = () => {
   questionCounter = 0;
+  score = 0;
   availableQuestions = [...questions];
   initial();
   getNewQuestions();
@@ -69,6 +74,7 @@ startQuiz = () => {
 
 getNewQuestions = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    // localStorage.setItem("mostRecentScore", score);
     //go to end page
     return window.location.assign("/end.html");
   }
@@ -78,6 +84,7 @@ getNewQuestions = () => {
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
 
+  //Update the progress bar
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
@@ -104,6 +111,10 @@ choices.forEach((choice) => {
     const classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
@@ -112,5 +123,12 @@ choices.forEach((choice) => {
     }, 1000);
   });
 });
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+console.log(score);
 
 startQuiz();
