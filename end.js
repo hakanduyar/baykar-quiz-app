e279babd7c2d516c8cc1111a1c59d41d7bda5e41
rikords <iamhakanduyar@gmail.com>
2024-03-16 23:26:47 +0300

    Update end.js and start.js to save the most recent score in localStorage.

--- end.js
+++ end.js
@@ -1,5 +1,12 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+localStorage.setItem("highScores", JSON.stringify([]));
+console.log(localStorage.getItem("highScores"));
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
--- start.js
+++ start.js
@@ -74,7 +74,7 @@ startQuiz = () => {
 
 getNewQuestions = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    // localStorage.setItem("mostRecentScore", score);
+    localStorage.setItem("mostRecentScore", score);
     //go to end page
     return window.location.assign("/end.html");
   }
