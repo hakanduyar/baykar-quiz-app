2928dca0841c09cfa93b0c17129f5fe01f040cf3
rikords <iamhakanduyar@gmail.com>
2024-03-17 03:48:37 +0300

    Update to save high scores to local storage.

--- end.js
+++ end.js
@@ -3,8 +3,9 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-localStorage.setItem("highScores", JSON.stringify([]));
-console.log(localStorage.getItem("highScores"));
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -13,6 +14,16 @@ username.addEventListener("keyup", () => {
 });
 
 saveHighScore = (e) => {
-  console.log("clicked the save button!");
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 };
